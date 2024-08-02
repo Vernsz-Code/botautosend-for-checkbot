bf9cc0351b7d54c1d23300781562a757eb9a8e05
Vernsz-Code <afycyt@gmail.com>
2024-08-02 08:39:29 +0700

    update feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const readline = require('readline');
 const cron = require('node-cron');
 const qrcode = require('qrcode-terminal');
+const numbers = ["6281272778236", "6285664914563"];
 const messages = [
     "Format Patrol Check #7 Mesin #7",
     "Format Patrol Check #8 Mesin #8",
@@ -13,23 +14,34 @@ const rl = readline.createInterface({
 });
 
 let table;
+let manualFetch;
 let messageIndex = 0;
 
 const askTableName = () => {
-    rl.question('Pilih Shift. Ketik pagi/sore/malam: ', (tableName) => {
+    rl.question('Pilih Shift. Ketik pagi/sore/malam/manual/custom: ', (tableName) => {
         if (tableName === "pagi") {
             table = "shift_pagi";
         } else if (tableName === "sore") {
             table = "shift_siang";
         } else if (tableName === "malam") {
             table = "shift_malam";
+        } else if (tableName === "custom") {
+            table = "custom";
+        } else if (tableName === "manual") {
+            rl.question('Input Menit: ', (minutes) => {
+                table = "manual";
+                manualFetch = minutes;
+                rl.close();
+                startProcess2();
+            });
+            return;
         } else {
             console.log("Invalid input!");
             rl.close();
             process.exit(1);
         }
         rl.close();
-        startProcess();
+        startProcess1();
     });
 };
 
@@ -56,6 +68,12 @@ const sendMessage = (number, message) => {
         console.error(`Error when sending message to ${number}:`, err);
     });
 };
+const formatTime = (date) => {
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+};
 
 const fetchMessagesToSend = () => {
     const now = new Date();
@@ -63,32 +81,51 @@ const fetchMessagesToSend = () => {
     const adjustedNow = new Date(now.getTime() + 7 * 60 * 60 * 1000);
     const adjustedOneMinuteAgo = new Date(oneMinuteAgo.getTime() + 7 * 60 * 60 * 1000);
 
-    const formattedNow = adjustedNow.toISOString().slice(0, 19).replace('T', ' ');
-    const formattedOneMinuteAgo = adjustedOneMinuteAgo.toISOString().slice(0, 19).replace('T', ' ');
+    const formattedNow = formatTime(adjustedNow);
+    const formattedOneMinuteAgo = formatTime(adjustedOneMinuteAgo);
 
-    const numbers = ["6281272778236", "6285664914563"];
-
+    console.log(formattedNow, formattedOneMinuteAgo)
     db.query(`SELECT * FROM ${table} WHERE waktu > ? AND waktu <= ?`, [formattedOneMinuteAgo, formattedNow], (err, results) => {
         if (err) {
             console.error('Error fetching messages:', err);
             return;
         }
-
         results.forEach(row => {
             const selectedMessage = messages[messageIndex];
-                sendMessage(numbers[1], selectedMessage);
+            sendMessage(numbers[0], selectedMessage);
             messageIndex = (messageIndex + 1) % messages.length;
-        });
+        })
     });
 };
 
-const startProcess = () => {
+const startProcess1 = () => {
     client.on('ready', () => {
         console.log('Client is ready!');
 
-        cron.schedule('* * * * *', () => {
-            console.log('Checking for messages to send at', new Date().toLocaleString());
+        cron.schedule('* * * * *', () => { // Run every minute
+            console.log('Checking for messages from sp1 to send at', new Date().toLocaleString());
             fetchMessagesToSend();
+        });
+    });
+
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+        console.log('QR Code generated. Please scan with your WhatsApp.');
+    });
+
+    client.initialize();
+};
+
+const startProcess2 = () => {
+    client.on('ready', () => {
+        console.log('Client is ready!');
+
+        cron.schedule(`*/${manualFetch} * * * *`, () => {
+            console.log('Checking for messages from sp2 to send at', new Date().toLocaleString());
+            const selectedMessage = messages[messageIndex];
+             console.log("ini dari sp2")
+            sendMessage(numbers[0], selectedMessage);
+            messageIndex = (messageIndex + 1) % messages.length;
         });
     });
 
